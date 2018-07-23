d2ddc5451a90e78c956b6378c712f52a9c677c03
Stefan Krastanov <stefan@krastanov.org>
2018-07-22 23:54:19 -0400

    Change LF clock source to SYNTH.
    The internal RC oscillator could cause connection issues if not properly
    calibrated. Instead, we just use the HF clock to synthesize the LF one.
    
    This results in much higher current consumption as we can not turn off
    the HF oscillator. Given that we are already keeping the FPU on all the
    time, this might be a negligible contribution though (needs to be
    checked).

--- custom_board/s132/config/sdk_config.h
+++ custom_board/s132/config/sdk_config.h
@@ -1240,7 +1240,7 @@
 // <2=> Synth 
 
 #ifndef CLOCK_CONFIG_LF_SRC
-#define CLOCK_CONFIG_LF_SRC 1
+#define CLOCK_CONFIG_LF_SRC 0
 #endif
 
 // <o> CLOCK_CONFIG_IRQ_PRIORITY  - Interrupt priority
@@ -1677,7 +1677,7 @@
 // <2=> Synth 
 
 #ifndef NRFX_CLOCK_CONFIG_LF_SRC
-#define NRFX_CLOCK_CONFIG_LF_SRC 1
+#define NRFX_CLOCK_CONFIG_LF_SRC 0
 #endif
 
 // <o> NRFX_CLOCK_CONFIG_IRQ_PRIORITY  - Interrupt priority
@@ -11945,12 +11945,12 @@
 // <2=> NRF_CLOCK_LF_SRC_SYNTH 
 
 #ifndef NRF_SDH_CLOCK_LF_SRC
-#define NRF_SDH_CLOCK_LF_SRC 0
+#define NRF_SDH_CLOCK_LF_SRC 2
 #endif
 
 // <o> NRF_SDH_CLOCK_LF_RC_CTIV - SoftDevice calibration timer interval. 
 #ifndef NRF_SDH_CLOCK_LF_RC_CTIV
-#define NRF_SDH_CLOCK_LF_RC_CTIV 22
+#define NRF_SDH_CLOCK_LF_RC_CTIV 0
 #endif
 
 // <o> NRF_SDH_CLOCK_LF_RC_TEMP_CTIV - SoftDevice calibration timer interval under constant temperature. 
