ac8f882b24ed875b647fb140920cbcad7cc17da3
Stefan Krastanov <stefan@krastanov.org>
2018-08-07 02:54:23 -0400

    Implement simple multilateration based on signal strength
    A buffer of fixed size (currently 100) is filled with measurements of beacon
    signal strength (the beacons announce their location) and used to calculate
    location.
    
    Description of the multilateration algorithm:
    
    Multilateration can be done based on (noisy) distance measurements to a
    set of beacons with known positions. The multilateration is performed by
    attempting to solve the system of nonlinear equations:
    
    (x[i]-X)^2 + (y[i]-Y)^2 = r[i]^2
    
    where x[i] and y[i] are the position of the i-th beacon and r[i] is
    the approximate distance to that beacon. X and Y are the unknown
    position of the receiver.
    
    The i=0 equation is substracted from the other equations in order to
    linearize the system, at which linear least squares is performed on the
    data.
    
    The list of beacons is stored in a ring buffer, in order to permit easy
    on-the-fly updates with newly discovered beacons.
    
    Thread locking is not implemented yet, but it is marked with TODO flags.

--- scanner.h
+++ scanner.h
@@ -1,3 +1,4 @@
+#include "multilateration.h"
 // TODO cleanup and documentation... certain uuids should be placed in a better, more central place
 
 #define APP_BLE_OBSERVER_PRIO   3    /**< Application's BLE observer priority. You shouldn't need to modify this value. */
@@ -6,12 +7,20 @@
 #define SCAN_INTERVAL   MSEC_TO_UNITS(40, UNIT_0_625_MS)
 #define SCAN_WINDOW     MSEC_TO_UNITS(30, UNIT_0_625_MS) // The window was to be smaller than the interval in order to have reliable advertisements while scanning.
 
+#define MULTILATERATION_BUFFER_SIZE 100
+#define INTERCEPT -64.19
+#define SLOPE -1.579
+#define MIN_DISTANCE 0.1
+#define MAX_DISTANCE 15
+
 static ble_gap_scan_params_t scan_params = {
     .extended=0, .report_incomplete_evts=0, .active=0, .filter_policy=BLE_GAP_SCAN_FP_ACCEPT_ALL, .scan_phys=BLE_GAP_PHY_AUTO,
     .interval=SCAN_INTERVAL, .window=SCAN_WINDOW,
     .timeout=BLE_GAP_SCAN_TIMEOUT_UNLIMITED};
 static uint8_t scan_buffer[200];
 static ble_data_t scan_data = {.p_data = scan_buffer, .len = 100};
+
+static multilateration_state_t m_multilatstate;
 
 static uint8_t scanner_check_uuid(ble_data_t * adv_data)
 {
@@ -45,9 +54,27 @@ static void scanner_ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_contex
             ble_gap_evt_adv_report_t const * report = &p_ble_evt->evt.gap_evt.params.adv_report;
             if (scanner_check_uuid(&report->data))
             {
-                NRF_LOG_DEBUG("ADV EVENT MAC %x:%x RSSI %d LEN %d DATA %d %d",
-                              report->peer_addr.addr[5], report->peer_addr.addr[4],
-                              report->rssi, report->data.len, report->data.p_data[report->data.len-2], report->data.p_data[report->data.len-1]);
+                //NRF_LOG_DEBUG("ADV EVENT MAC %x:%x RSSI %d LEN %d DATA %d %d",
+                //              report->peer_addr.addr[5], report->peer_addr.addr[4],
+                //              report->rssi, report->data.len, report->data.p_data[report->data.len-2], report->data.p_data[report->data.len-1]);
+                float distance = (report->rssi-INTERCEPT)/SLOPE;
+                if (distance<MAX_DISTANCE) {
+                    if (distance<MIN_DISTANCE) distance=MIN_DISTANCE;
+                    multilateration_insert(&m_multilatstate,
+                                           report->data.p_data[report->data.len-2],
+                                           report->data.p_data[report->data.len-1],
+                                           distance);
+                    if (m_multilatstate.start==0 && m_multilatstate.stop==MULTILATERATION_BUFFER_SIZE-1) {
+                        uint32_t retcode = multilateration(&m_multilatstate);
+                        float X,Y,Xn,Yn;
+                        uint32_t retcode1 = multilateration_getnearest(&m_multilatstate, &Xn, &Yn);
+                        uint32_t retcode2 = multilateration_getXY(&m_multilatstate, &X, &Y);
+                        NRF_LOG_DEBUG("error code %d %d %d", retcode, retcode1, retcode2);
+                        NRF_LOG_DEBUG("estimated loc  : X " NRF_LOG_FLOAT_MARKER " Y " NRF_LOG_FLOAT_MARKER, NRF_LOG_FLOAT(X), NRF_LOG_FLOAT(Y));
+                        NRF_LOG_DEBUG("closest beacon : X " NRF_LOG_FLOAT_MARKER " Y " NRF_LOG_FLOAT_MARKER, NRF_LOG_FLOAT(Xn), NRF_LOG_FLOAT(Yn));
+                    }
+                }
+
             }
             sd_ble_gap_scan_start(NULL, &scan_data);
         } break;
@@ -60,6 +87,7 @@ static void scanner_ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_contex
 void scanner_init()
 {
     ret_code_t err_code;
+    multilateration_init(&m_multilatstate, MULTILATERATION_BUFFER_SIZE); // TODO error check
     err_code = sd_ble_gap_scan_start(&scan_params,&scan_data);
     APP_ERROR_CHECK(err_code);
     NRF_SDH_BLE_OBSERVER(m_ble_scanner, APP_BLE_OBSERVER_PRIO, scanner_ble_evt_handler, NULL);
